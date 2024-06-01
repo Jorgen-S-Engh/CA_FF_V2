e92951fd584c3342a8dceb73710018bf4366a724
Jørgen Engh <jorgen.engh@hotmail.com>
2024-06-01 10:25:42 +0200

    fixes counter on shopping cart

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,8 +3,10 @@ import { Outlet, Link } from "react-router-dom";
 import Avatar from "@mui/material/Avatar";
 import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 import { Box } from "@mui/material";
+import useStore from "../store";
 
 function Layout() {
+  const count = useStore((state) => state.count);
   return (
     <>
       <Box
@@ -38,15 +40,34 @@ function Layout() {
             marginLeft: "auto",
           }}
         >
-          <Avatar>
-            <ShoppingBagIcon color="primary" />
-            <Box
-              sx={{
-                color: " black",
-                fontWeight: "bold",
-              }}
-            ></Box>
-          </Avatar>
+          <Box
+            sx={{
+              position: "relative",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <ShoppingBagIcon color="primary" sx={{ fontSize: 45 }} />
+
+            {count > 0 ? (
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: "25px",
+                  bgcolor: "gray",
+                  borderRadius: "50%",
+                  textAlign: "center",
+                  width: "25px",
+                  fontWeight: "bold",
+                }}
+              >
+                {count}
+              </Box>
+            ) : (
+              ""
+            )}
+          </Box>
         </div>
       </Box>
       <Outlet />
