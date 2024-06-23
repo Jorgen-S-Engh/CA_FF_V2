058f20b793aab3dcd0104ab3e35fd3afabef7db0
Jørgen Engh <jorgen.engh@hotmail.com>
2024-06-23 20:02:33 +0200

    update store

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,13 +5,16 @@ import axios from "axios";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import useStore from "../store";
+import DropdownExample from "../components/DropDown";
+import Grid from "@mui/material/Unstable_Grid2";
+import Typography from "@mui/material/Typography";
 
 function Product() {
   const { id } = useParams();
   const [productData, setProductData] = useState(null);
-  const increase = useStore((state) => state.increase);
-  const decrease = useStore((state) => state.decrease);
-  const count = useStore((state) => state.count);
+  const addToCart = useStore((state) => state.addToCart);
+  // const increase = useStore((state) => state.increase);
+  // const decrease = useStore((state) => state.decrease);
 
   const getProductData = async () => {
     try {
@@ -36,29 +39,80 @@ function Product() {
     return <div>loading...</div>;
   }
 
+  let priceContainer = (
+    <Box>
+      <Typography variant="h5">{Math.round(productData.price)} kr</Typography>
+    </Box>
+  );
+
+  if (productData.price != productData.discountedPrice) {
+    priceContainer = (
+      <Box>
+        <Typography variant="h5">
+          {Math.round(productData.discountedPrice)} Kr
+        </Typography>
+        <Typography style={{ textDecoration: "line-through" }}>
+          Before: {Math.round(productData.price)} kr
+        </Typography>
+      </Box>
+    );
+  }
+
   return (
-    <div>
-      <Box
+    <Box
+      sx={{
+        padding: "50px",
+        display: "flex",
+        justifyContent: "start",
+      }}
+    >
+      <Grid
+        container
+        spacing={2}
         sx={{
-          display: "flex",
+          width: "100%",
+          bgcolor: "rgba(250, 249, 241, 0.8)",
+          padding: "30px",
         }}
       >
-        <img
-          style={{
-            width: "400px",
-          }}
-          src={productData.imageUrl}
-          alt=""
-        />
-        <Box>
-          <h1>{productData.title}</h1>
-        </Box>
-      </Box>
-      <Button onClick={increase}>add to cart</Button>
+        <Grid xs={6}>
+          <Box>
+            <img
+              style={{ maxWidth: "100%" }}
+              src={productData.imageUrl}
+              alt=""
+            />
+          </Box>
+        </Grid>
+        <Grid xs={6}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <h1>{productData.title}</h1>
+            <Typography sx={{}}>{productData.description}</Typography>
+          </Box>
 
-      {count}
-      <Button onClick={decrease}> remove from cart</Button>
-    </div>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <span>{priceContainer}</span>
+            {/* <Typography variant="h4">
+              {productData.price === productData.discountedPrice
+                ? productData.price
+                : productData.discountedPrice}
+            </Typography> */}
+            <DropdownExample />
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
